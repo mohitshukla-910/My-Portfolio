51869b643dedb696228a9127157323ac53e2f1e6
Mohit Shukla <mohitshukla9102@gmail.com>
2025-07-20 05:41:48 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false)
 
   const navigation = [
-    { name: 'Home', href: '#hero' },
+    { name: 'Home', href: '#home' },
     { name: 'About', href: '#about' },
     { name: 'Skills', href: '#skills' },
     { name: 'Projects', href: '#projects' },
